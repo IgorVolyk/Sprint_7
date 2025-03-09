7b0ba7114af86ae75de9fa2f9712724b50c2fc82
Igor <igorwspb@yahoo.com>
2025-03-09 12:30:56 +0300

    second iteration

--- main_test.go
+++ main_test.go
@@ -3,48 +3,9 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandlerWhenOk(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
@@ -59,16 +20,16 @@ func TestMainHandlerWhenOk(t *testing.T) {
 }
 
 func TestMainHandler_WrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=1&city=spb", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=1&city=murmansk", nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("expected %d, got %d", http.StatusBadRequest, status)
+		t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
 	}
-	if body := responseRecorder.Body.String(); body != "wrong city value" {
-		t.Errorf("expected 'wrong city value', got %s", body)
+	if body := responseRecorder.Body.String(); body != "wrong city" {
+		t.Errorf("expected 'wrong city', got %s", body)
 	}
 }
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
